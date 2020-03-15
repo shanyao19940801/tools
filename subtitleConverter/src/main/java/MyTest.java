7f593fe6e47e05b8cac8e0faba0fc1e7fb86f234
shanyao <sqshanyao@163.com>
2020-03-15 22:10:04 +0800

    获取全部字幕

--- subtitleConverter/src/main/java/MyTest.java
+++ subtitleConverter/src/main/java/MyTest.java
@@ -12,6 +12,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -21,7 +23,7 @@ public class MyTest {
     public static void main(String[] args) {
         List<FileInfoBean> filePath = GetAllFileName.getFilePath("F:\\视频剪辑\\各种平台\\电影\\漫威\\字幕");
         System.out.println(filePath);
-        formatSRT();
+        formatSRT(filePath);
 
     }
 
@@ -43,21 +45,68 @@ public class MyTest {
         }
     }
 
-    public static void formatSRT() {
-        try {
-            TimedTextObject tto;
-            TimedTextFileFormat ttff;
-            OutputStream output = null;
+    public static void formatSRT(List<FileInfoBean> filePath) {
+        String[] all = new String[0];
+        for (FileInfoBean bean : filePath) {
+            try {
+                TimedTextObject tto;
+                TimedTextFileFormat ttff;
+                OutputStream output = null;
 
-            ttff = new FormatSRTUtil();
-            File file = new File("F:\\work\\testfile\\钢铁侠1.srt");
-            InputStream is = new FileInputStream(file);
-            tto = ttff.parseFile(file.getName(), is);
-            IOClass.writeFileTxt("test.srt", tto.toSRT());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (FatalParsingException e) {
-            e.printStackTrace();
+                ttff = new FormatSRTUtil();
+                File file = new File(bean.getFilePath());
+                InputStream is = new FileInputStream(bean.getFilePath());
+                tto = ttff.parseFile(file.getName(), is);
+                String[] fileName = {"MoveName:" + bean.getFileName()};
+                all = concat(all, fileName, tto.toSRT());
+
+//                IOClass.writeFileTxt("test.srt", tto.toSRT());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (FatalParsingException e) {
+                e.printStackTrace();
+            }
         }
+        System.out.println(all);
+        IOClass.writeFileTxt("all.srt", all);
     }
+
+    public static <T> T[] concat(T[] first, T[]... rest) {
+        int totalLength = 0;
+        T[] result = null,temp = null;
+        if(first!=null && first.length>0) {
+            totalLength = first.length;
+        }
+        if (totalLength == 0){
+            for (T[] array : rest) {
+                if(array != null && array.length>0) {
+                    if (totalLength == 0){
+                        temp = array;
+                        totalLength += array.length;
+                    }else {
+                        temp = concat(temp,array);
+                    }
+                }
+            }
+            result = temp;
+        }else {
+            for (T[] array : rest) {
+                if(array!=null && array.length>0) {
+                    totalLength += array.length;
+                }
+            }
+            if (totalLength != 0){
+                result = Arrays.copyOf(first, totalLength);
+                int offset = first.length;
+                for (T[] array : rest) {
+                    if(array!=null && array.length>0) {
+                        System.arraycopy(array, 0, result, offset, array.length);
+                        offset += array.length;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
 }
